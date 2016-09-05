9ddd371ec2c7c5066d2da07b1ea6eb44e13c2e28
Oracle <daniel.ehrlich1@gmail.com>
2016-09-05 15:45:14 -0500

    Added Todo list

--- src/components/app.js
+++ src/components/app.js
@@ -7,10 +7,10 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
+        <div>
         <SearchBar/>
         <WeatherList/>
-      </div>
+        </div>
     );
   }
 }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,12 +1,13 @@
-import { FETCH_WEATHER } from '../actions/index';
 
 export default function (state = [], action) {
 
     switch (action.type) {
-        case FETCH_WEATHER:
+        case 'FETCH_WEATHER':
             return [action.payload.data, ...state];
     }
 
     console.log("Action Received", action);
     return state;
 }
+
+//import { FETCH_WEATHER } from '../actions/index';
