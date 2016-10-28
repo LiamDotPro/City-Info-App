a724011401cde0f0d29c3ce46d484110eb59afb7
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2016-10-27 21:30:13 -0500

    Set up framework to include own Google API key to increase limit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
 import loadGoogleMapsAPI from 'load-google-maps-api';
+// import gmapAPIKEY from './dev_api_key.js';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -12,7 +13,8 @@ import './style/vendor/bootstrap.css';
 import './style/style.css';
 import './style/title.css';
 
-
+const devAPIKey = ""; // get a variable from a file here...
+const options = { "key" : process.env.gmapAPI || 'devAPIkeyhere' };
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 loadGoogleMapsAPI().then((googleMaps) => {
   console.log('success'); //= > Object { Animation: Object, ...
