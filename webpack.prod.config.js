0a7d200f14c402723863b523b75fdfcd2d97004c
Daniel Ehrlich <daniel.ehrlich1@gmail.com>
2016-12-14 19:05:33 -0600

    removed css sourcemap

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -29,7 +29,7 @@ module.exports = {
       
       {
         test  : /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
+        loader: ExtractTextPlugin.extract('style', 'css')
       }
     
     ]
